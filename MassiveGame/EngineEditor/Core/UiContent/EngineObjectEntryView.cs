b9dc68a58d6f8916cce1bdfb432a037f04f68222
Dem <demusszinov@gmail.com>
2018-12-10 11:55:12 +0200

    Fix for resource exception

--- MassiveGame/EngineEditor/Core/UiContent/EngineObjectEntryView.cs
+++ MassiveGame/EngineEditor/Core/UiContent/EngineObjectEntryView.cs
@@ -12,21 +12,21 @@ namespace MassiveGame.EngineEditor.Core.UiContent
         {
             var entries = new ObservableCollection<EngineObjectEntry>
             {
-                new EngineObjectEntry { EntryLabel = "Entry 1", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 2", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 3", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 4", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 5", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 6", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 7", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 8", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 9", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 10", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 11", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 12", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 13", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 14", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" },
-                new EngineObjectEntry { EntryLabel = "Entry 15", IconURI = ProjectFolders.EditorTexturePath + "icon2.png" }
+                new EngineObjectEntry { EntryLabel = "Entry 1", IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 2", IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 3", IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 4", IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 5", IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 6", IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 7", IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 8", IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 9", IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 10",IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 11",IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 12",IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 13",IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 14",IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" },
+                new EngineObjectEntry { EntryLabel = "Entry 15",IconURI = "C:\\Users\\dzinovev\\Desktop\\Универчик\\project\\HighlightEngine\\HighlightEngineUI\\Images\\icon2.png" }
             };
             EngineObjectEntries = entries;
         }
